94e4103c9bf97fb173daa0bd91330c5d8639cbbb
Monika <mika.pikuzicha@vp.pl>
2019-11-16 18:06:00 +0100

    Home page plus half of subpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-const h1 = document.querySelector('h1')
-const header = document.querySelector('header')
+const h1 = document.querySelector('h1');
+const header = document.querySelector('header');
+const section = document.querySelector('section');
 
 h1.addEventListener('click', () => {
     if (header.classList.contains('hidden')) {
@@ -11,6 +12,7 @@ h1.addEventListener('click', () => {
         header.classList.add('visuallyhidden');
         header.addEventListener('transitionend', function (e) {
             header.classList.add('hidden');
+            section.classList.remove('hidden');
         }, {
             capture: false,
             once: true,
